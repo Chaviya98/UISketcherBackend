1fbdd32810b413deaa810a5fd1ce53b453f7f84b
chim_98 <chavividusha@gmail.com>
2021-02-12 13:56:17 +0530

    code refactored to spacy version 3.0 Lemmatisation added to support attributes

--- app.py
+++ app.py
@@ -1,56 +1,82 @@
 from flask import Flask, request
 import spacy
-from spacy.pipeline import EntityRuler
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 nlp = spacy.load("en_core_web_sm")
-ruler = EntityRuler(nlp)
+lemmatizer = nlp.get_pipe("lemmatizer")
+ruler = nlp.add_pipe("entity_ruler")
+# supported UI elements
 patterns = [{"label": "UI", "pattern": "button"}, {"label": "UI", "pattern": "textinput"},
             {"label": "UI", "pattern": "card"}, {"label": "UI", "pattern": "imageview"},
             {"label": "UI", "pattern": "header"}, {"label": "UI", "pattern": "textview"}]
 ruler.add_patterns(patterns)
-nlp.add_pipe(ruler)
 all_stopwords = nlp.Defaults.stop_words
+
+# supported attributes
+attribute_list = ['title', 'placeholder']
 
 
 @app.route('/extract/data', methods=['GET', 'POST'])
 def extract_data():
+    # Preparing data for tokenizing
+    doc = nlp(" ".join(request.data.decode('UTF-8').lower().split()))
 
-        # Preparing data for tokenizing
-        doc = nlp(" ".join(request.data.decode('UTF-8').lower().split()))
+    # Lemmatisation process
+    doc_after_lemmatizer = nlp(' '.join(map(str, [token.lemma_ for token in doc])))
 
-        # Tokenizing , stop words removing and unnecessary characters removing
-        tokens = [token.text for token in doc if not token.is_punct if not token.is_stop]
+    # Tokenizing , stop words removing and unnecessary characters removing
+    tokens = [token.text for token in doc_after_lemmatizer if not token.is_punct if not token.is_stop]
 
-        # Converting list of token into string
-        list_to_str = ' '.join(map(str, tokens))
+    # Preparing to entity recognition
+    doc_after_text_processing = nlp(' '.join(map(str, tokens)))
 
-        # Preparing to entity recognition
-        doc2 = nlp(list_to_str)
+    # entity recognition
+    entity_recognition = [(ent.text, ent.label_) for ent in doc_after_text_processing.ents]
 
-        # entity recognition
-        entity_recognition = [(ent.text, ent.label_) for ent in doc2.ents]
+    # Extraction of attribute value if there is any
+    attribute_value = extract_attribute(attribute_list, tokens)
 
-        # json_format = json.dumps(entity_recognition)
-
-        if len(entity_recognition) == 0:
-            print("No UI elements detected")
-
+    if len(entity_recognition) == 0:
+        response_type = {
+            "data": {
+                "element": "",
+                "attribute": ""
+            },
+            "error": "No UI elements detected"
+        }
+        return response_type
+    else:
+        if len(entity_recognition) < 2:
+            # Preparing the response json
+            response_type = {
+                "data": {
+                    "element": ' '.join(map(str, [ent.text for ent in doc_after_text_processing.ents])),
+                    "attribute": attribute_value
+                },
+                "error": ""
+            }
+            return response_type
         else:
-            if len(entity_recognition) < 2:
-                # Preparing the response json
-                response_type = {
-                    "data": {
-                        "element": ent.text for ent in doc2.ents
-                    }
-                }
-                return response_type
-            else:
-                print("Multiple UI Element Detected")
+            response_type = {
+                "data": {
+                    "element": "",
+                    "attribute": ""
+                },
+                "error": "Multiple UI Element Detected"
+            }
+            return response_type
+
+
+def extract_attribute(attributes, tokens):
+    for t in attributes:
+        for term in tokens:
+            if term != t:
+                pass
+            elif term == t:
+                return tokens[tokens.index(term) + 1]
 
 
 if __name__ == '__main__':
     app.run()
-
